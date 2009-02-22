8ecb23309e2386ee3568d021bbdabe5944108e34
Peter Eschler <peschler@gmail.com>
2009-02-22 22:56:53 +0000

    Updated documentation and added some initial distutils setup file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-modeltranslation',
+      version='0.1',
+      description='Translates Django models using a registration approach.',
+      author='Peter Eschler',
+      author_email='peschler@googlemail.com',
+      url='http://code.google.com/p/django-modeltranslation/',
+      packages=['modeltranslation', 'modeltranslation.management', 'modeltranslation.management.commands'],
+     )
