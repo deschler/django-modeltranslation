4b564e147aefbaa68a5f26d541479037c164ce47
Dirk Eschler <eschler@gmail.com>
2010-02-23 14:44:23 +0000

    Removed print statements so its usable with mod_wsgi. Resolves issue 7.

--- modeltranslation/models.py
+++ modeltranslation/models.py
@@ -1,7 +1,6 @@
-
+# -*- coding: utf-8 -*-
 from django.db import models
 from django.conf import settings
-
 from modeltranslation.translator import translator
 
 # Every model registered with the modeltranslation.translator.translator
@@ -12,14 +11,17 @@ from modeltranslation.translator import translator
 # classes and their translation options with the translator object. 
 # This requires an extra settings entry, because I see no other way
 # to determine the module name of the project
+# TODO: Consider removal of TRANSLATION_REGISTRY
+#       It is supposed to be kept in the project's root and this has to be in
+#       PYTHONPATH anyway.
 try: 
     translation_mod = __import__(settings.TRANSLATION_REGISTRY, {}, {}, [''])
 except ImportError, exc:
-    print "No translation.py found in the project directory."
-    #raise ImportError("No translation.py found in the project directory.")
+    raise ImportError("modeltranslation: TRANSLATION_REGISTRY '%s' not found." 
+                      % settings.TRANSLATION_REGISTRY)
 
 # After importing all translation modules, all translation classes are 
-# registered with the translator. 
-translated_app_names = ', '.join(t.__name__ for t in translator._registry.keys())
-print "modeltranslation: registered %d applications for translation (%s)." % (len(translator._registry),
-                                                                              translated_app_names)
+# registered with the translator.
+if settings.DEBUG:
+    translated_app_names = ', '.join(t.__name__ for t in translator._registry.keys())
+    print "modeltranslation: registered %d applications for translation (%s)." % (len(translator._registry), translated_app_names)
