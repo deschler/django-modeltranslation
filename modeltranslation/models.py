37e0ec21d550249cecc5d028ba77009a45a8adb2
Dirk Eschler <eschler@gmail.com>
2010-03-02 15:34:49 +0000

    Removed TRANSLATION_REGISTRY setting. The translation.py is supposed to be kept in the project root and this has to be in PYTHONPATH anyway.

--- modeltranslation/models.py
+++ modeltranslation/models.py
@@ -9,19 +9,18 @@ from modeltranslation.translator import translator
 
 # Import the project's global "translation.py" which registers model 
 # classes and their translation options with the translator object. 
-# This requires an extra settings entry, because I see no other way
-# to determine the module name of the project
-# TODO: Consider removal of TRANSLATION_REGISTRY
-#       It is supposed to be kept in the project's root and this has to be in
-#       PYTHONPATH anyway.
 try: 
-    translation_mod = __import__(settings.TRANSLATION_REGISTRY, {}, {}, [''])
-except ImportError, exc:
-    raise ImportError("modeltranslation: TRANSLATION_REGISTRY '%s' not found." 
-                      % settings.TRANSLATION_REGISTRY)
+    import translation
+except ImportError:
+    import sys
+    sys.stderr.write("modeltranslation: Error can't find the file " \
+                     "'translation.py' in your project root.\n")
+    sys.exit(1)
 
 # After importing all translation modules, all translation classes are 
 # registered with the translator.
 if settings.DEBUG:
-    translated_app_names = ', '.join(t.__name__ for t in translator._registry.keys())
-    print "modeltranslation: registered %d applications for translation (%s)." % (len(translator._registry), translated_app_names)
+    translated_model_names = ', '.join(
+        t.__name__ for t in translator._registry.keys())
+    print "modeltranslation: Registered %d models for translation (%s)." % (
+        len(translator._registry), translated_model_names)
