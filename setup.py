33d780ff7e69a43bf4a06051d1c7bf47e5adf157
Dirk Eschler <eschler@gmail.com>
2010-06-15 10:12:06 +0000

    Prepared 0.2 release.

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='django-modeltranslation',
-      version='0.1',
+      version='0.2',
       description='Translates Django models using a registration approach.',
-      author='Peter Eschler',
-      author_email='peschler@googlemail.com',
+      author='Peter Eschler, Dirk Eschler',
+      author_email='peschler@googlemail.com, eschler@gmail.com',
       url='http://code.google.com/p/django-modeltranslation/',
-      packages=['modeltranslation', 'modeltranslation.management', 'modeltranslation.management.commands'],
-      liicense='New BSD' 
-     )
+      packages=['modeltranslation', 'modeltranslation.management',
+                'modeltranslation.management.commands'],
+      license='New BSD')
