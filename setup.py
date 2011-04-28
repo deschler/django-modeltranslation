a5ce7658797c03d15635e7953bc9bedeac55d715
Dirk Eschler <eschler@gmail.com>
2011-04-28 14:33:25 +0000

    Added additional meta-data.

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python
-
 from distutils.core import setup
 
 setup(name='django-modeltranslation',
       version='0.2',
       description='Translates Django models using a registration approach.',
+      long_description='The modeltranslation application can be used to '
+                       'translate dynamic content of existing models to an '
+                       'arbitrary number of languages without having to '
+                       'change the original model classes. It uses a '
+                       'registration approach (comparable to Django\'s admin '
+                       'app) to be able to add translations to existing or '
+                       'new projects and is fully integrated into the Django '
+                       'admin backend.',
       author='Peter Eschler, Dirk Eschler',
-      author_email='peschler@googlemail.com, eschler@gmail.com',
+      author_email='p.eschler@nmy.de, d.eschler@nmy.de',
+      maintainer='Dirk Eschler',
+      maintainer_email='d.eschler@nmy.de',
       url='http://code.google.com/p/django-modeltranslation/',
       packages=['modeltranslation', 'modeltranslation.management',
                 'modeltranslation.management.commands'],
+      requires=['django(>=1.0)'],
+      classifiers=['Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License'],
       license='New BSD')
