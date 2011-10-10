6055a1daec087c454879d9071ce12d4076264a46
Dirk Eschler <eschler@gmail.com>
2011-10-10 18:13:20 +0000

    Fixed tabs don't properly support two-part language codes. Resolves issue 63.

--- modeltranslation/static/modeltranslation/js/tabbed_translation_fields.js
+++ modeltranslation/static/modeltranslation/js/tabbed_translation_fields.js
@@ -25,9 +25,18 @@ var google, django, gettext;
               grouped_translations = {};
 
             translation_fields.each(function (i, el) {
-                var name = $(el).attr('name').split('_'),
-                  lang = name.pop();
-                name = name.join('_');
+                // Extract fieldname and original language code from class attribute
+                var css_lang_suffix = 'modeltranslation-field-';
+                var name = '';
+                var lang = '';
+                $.each($(el).attr('class').split(' '), function(j, cls) {
+                    if (cls.substring(0, css_lang_suffix.length) === css_lang_suffix) {
+                        var v = cls.substring(css_lang_suffix.length,
+                                              cls.length).split('__');
+                        name = v[0];
+                        lang = v[1];
+                    }
+                });
                 if (!grouped_translations[name]) {
                     grouped_translations[name] = {};
                 }
