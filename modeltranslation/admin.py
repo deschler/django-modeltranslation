74b7feaeffeeee589df2c135312cfa8c70241ca3
Dirk Eschler <eschler@gmail.com>
2012-07-10 14:59:24 +0000

    Removed stale models.py import.

--- modeltranslation/admin.py
+++ modeltranslation/admin.py
@@ -9,10 +9,6 @@ from modeltranslation.settings import DEFAULT_LANGUAGE
 from modeltranslation.translator import translator
 from modeltranslation.utils import (get_translation_fields,
                                     build_localized_fieldname)
-# Ensure that models are registered for translation before TranslationAdmin
-# runs. The import is supposed to resolve a race condition between model import
-# and translation registration in production (see issue 19).
-import modeltranslation.models
 
 
 class TranslationBaseModelAdmin(BaseModelAdmin):
