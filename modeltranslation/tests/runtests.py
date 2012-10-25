6fb0940273f5b9442a41673c41836fcbf58e080a
Dirk Eschler <eschler@gmail.com>
2012-10-25 11:32:16 +0200

    Created DjangoTestSuiteRunner manually to see if it changes anything when running on travis-ci.

--- modeltranslation/tests/runtests.py
+++ modeltranslation/tests/runtests.py
@@ -2,18 +2,18 @@
 import os
 import sys
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-#test_dir = os.path.join(
-#    os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'tests')
-#sys.path.insert(0, test_dir)
 
-from django.test.utils import get_runner
-from django.conf import settings
+from django.test.simple import DjangoTestSuiteRunner
+#from django.test.utils import get_runner
+#from django.conf import settings
 
 
 def runtests():
-    TestRunner = get_runner(settings)
-    test_runner = TestRunner(verbosity=2, failfast=False)
-    failures = test_runner.run_tests(['modeltranslation'])
+    #TestRunner = get_runner(settings)
+    #test_runner = TestRunner(verbosity=2, failfast=False)
+    #failures = test_runner.run_tests(['modeltranslation'])
+    failures = DjangoTestSuiteRunner(
+        verbosity=2, failfast=False).run_tests(['modeltranslation'])
     sys.exit(bool(failures))
 
 
