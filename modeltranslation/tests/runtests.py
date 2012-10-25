b361b87848d99ebcd2b6a70a0ad21b9138350e79
Dirk Eschler <eschler@gmail.com>
2012-10-25 12:07:40 +0200

    Trying yet another approach to make things work on travis-ci using management command to call test.

--- modeltranslation/tests/runtests.py
+++ modeltranslation/tests/runtests.py
@@ -3,17 +3,21 @@ import os
 import sys
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
-from django.test.simple import DjangoTestSuiteRunner
+#from django.test.simple import DjangoTestSuiteRunner
 #from django.test.utils import get_runner
 #from django.conf import settings
+from django.core.management import call_command
 
 
 def runtests():
-    #TestRunner = get_runner(settings)
-    #test_runner = TestRunner(verbosity=2, failfast=False)
-    #failures = test_runner.run_tests(['modeltranslation'])
-    failures = DjangoTestSuiteRunner(
-        verbosity=2, failfast=False).run_tests(['modeltranslation'])
+#    #TestRunner = get_runner(settings)
+#    #test_runner = TestRunner(verbosity=2, failfast=False)
+#    #failures = test_runner.run_tests(['modeltranslation'])
+#    failures = DjangoTestSuiteRunner(
+#        verbosity=2, failfast=False).run_tests(['modeltranslation'])
+#    sys.exit(bool(failures))
+    failures = call_command(
+        'test', 'modeltranslation', verbosity=2, failfast=False)
     sys.exit(bool(failures))
 
 
