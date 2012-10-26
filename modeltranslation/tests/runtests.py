508eb6e800e712b124c004065dd2c63fb6e37cf7
Dirk Eschler <eschler@gmail.com>
2012-10-26 13:21:30 +0200

    Debugging drop tables when running on travis-ci.

--- modeltranslation/tests/runtests.py
+++ modeltranslation/tests/runtests.py
@@ -16,6 +16,20 @@ def runtests():
 #    failures = DjangoTestSuiteRunner(
 #        verbosity=2, failfast=False).run_tests(['modeltranslation'])
 #    sys.exit(bool(failures))
+    # FIXME: Debugging drop tables when running on travis-ci
+    from django.db import connection
+    cursor = connection.cursor()
+    tables = ['tests_testmodelwithfallback', 'tests_testmodelwithfallback2',
+              'tests_testmodelwithfilefields', 'tests_testmodelmultitablea',
+              'tests_testmodelmultitableb', 'tests_testmodelmultitablec',
+              'tests_testmodelmultitabled', 'tests_testmodelabstractb',
+              'tests_datamodel']
+    for table in tables:
+        try:
+            cursor.execute("DROP TABLE %s" % table)
+        except Exception, e:
+            print "DROP TABLE %s: %s" % (table, e)
+
     failures = call_command(
         'test', 'modeltranslation', interactive=False, failfast=False,
         verbosity=2)
