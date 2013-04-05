2d63001edd0489e8d77065ebbcafdb5e3c95033c
deschler <eschler@gmail.com>
2013-04-05 02:14:59 +0200

    Added convenience admin classes for easier inclusion of tabbed translation fields static files (closes #171).

--- modeltranslation/admin.py
+++ modeltranslation/admin.py
@@ -305,3 +305,28 @@ class TranslationGenericTabularInline(TranslationInlineModelAdmin, generic.Gener
 
 class TranslationGenericStackedInline(TranslationInlineModelAdmin, generic.GenericStackedInline):
     pass
+
+
+class TabbedDjangoJqueryTranslationAdmin(TranslationAdmin):
+    class Media:
+        js = (
+            'modeltranslation/js/force_jquery.js',
+            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/jquery-ui.min.js',
+            'modeltranslation/js/tabbed_translation_fields.js',
+        )
+        css = {
+            'all': ('modeltranslation/css/tabbed_translation_fields.css',),
+        }
+TabbedTranslationAdmin = TabbedDjangoJqueryTranslationAdmin
+
+
+class TabbedExternalJqueryTranslationAdmin(TranslationAdmin):
+    class Media:
+        js = (
+            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+            'modeltranslation/js/tabbed_translation_fields.js',
+        )
+        css = {
+            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
+        }
