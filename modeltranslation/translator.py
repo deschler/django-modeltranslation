69f6bfc75f0b3a6c3d4e7b36bde33cab570bc846
Kudlaty <kfuks2@o2.pl>
2013-04-07 00:07:30 +0200

    Use six.with_metaclass instead of some hack.

--- modeltranslation/translator.py
+++ modeltranslation/translator.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
+from django.utils.six import with_metaclass
 from django.db.models import Manager, ForeignKey
 from django.db.models.base import ModelBase
 from django.db.models.signals import post_init
@@ -35,10 +36,9 @@ class FieldsAggregationMetaClass(type):
                 attrs['fields'].update(base.fields)
         attrs['fields'] = tuple(attrs['fields'])
         return super(FieldsAggregationMetaClass, cls).__new__(cls, name, bases, attrs)
-TranslationOptionsClass = FieldsAggregationMetaClass('TranslationOptions', (object,), {})
 
 
-class TranslationOptions(TranslationOptionsClass):
+class TranslationOptions(with_metaclass(FieldsAggregationMetaClass, object)):
     """
     Translatable fields are declared by registering a model using
     ``TranslationOptions`` class with appropriate ``fields`` attribute.
