1d7e0876ecbec36bfecaec0cd72827c5e75f9613
Jacek Tomaszewski <jacek.tomek@gmail.com>
2014-03-27 00:39:33 +0100

    Satisfy new flake8 version.

--- modeltranslation/tests/translation.py
+++ modeltranslation/tests/translation.py
@@ -22,14 +22,14 @@ class UniqueNullableTranslationOptions(TranslationOptions):
 translator.register(UniqueNullableModel, UniqueNullableTranslationOptions)
 
 
-########## Proxy model testing
+# ######### Proxy model testing
 
 class ProxyTestTranslationOptions(TranslationOptions):
     fields = ('title', 'text', 'url', 'email',)
 translator.register(ProxyTestModel, ProxyTestTranslationOptions)
 
 
-########## Fallback values testing
+# ######### Fallback values testing
 
 class FallbackModelTranslationOptions(TranslationOptions):
     fields = ('title', 'text', 'url', 'email', 'description')
@@ -44,14 +44,14 @@ class FallbackModel2TranslationOptions(TranslationOptions):
 translator.register(FallbackModel2, FallbackModel2TranslationOptions)
 
 
-########## File fields testing
+# ######### File fields testing
 
 class FileFieldsModelTranslationOptions(TranslationOptions):
     fields = ('title', 'file', 'file2', 'image',)
 translator.register(FileFieldsModel, FileFieldsModelTranslationOptions)
 
 
-########## Foreign Key / OneToOneField testing
+# ######### Foreign Key / OneToOneField testing
 
 class ForeignKeyModelTranslationOptions(TranslationOptions):
     fields = ('title', 'test', 'optional', 'hidden', 'non',)
@@ -63,11 +63,11 @@ class OneToOneFieldModelTranslationOptions(TranslationOptions):
 translator.register(OneToOneFieldModel, OneToOneFieldModelTranslationOptions)
 
 
-########## Custom fields testing
+# ######### Custom fields testing
 
 class OtherFieldsModelTranslationOptions(TranslationOptions):
-#    fields = ('int', 'boolean', 'nullboolean', 'csi', 'float', 'decimal',
-#              'ip', 'genericip')
+    # fields = ('int', 'boolean', 'nullboolean', 'csi', 'float', 'decimal',
+    #           'ip', 'genericip')
     fields = ('int', 'boolean', 'nullboolean', 'csi', 'float', 'decimal',
               'ip', 'date', 'datetime', 'time',)
 translator.register(OtherFieldsModel, OtherFieldsModelTranslationOptions)
@@ -78,7 +78,7 @@ class DescriptorModelTranslationOptions(TranslationOptions):
 translator.register(DescriptorModel, DescriptorModelTranslationOptions)
 
 
-########## Multitable inheritance testing
+# ######### Multitable inheritance testing
 
 class MultitableModelATranslationOptions(TranslationOptions):
     fields = ('titlea',)
@@ -95,7 +95,7 @@ class MultitableModelCTranslationOptions(TranslationOptions):
 translator.register(MultitableModelC, MultitableModelCTranslationOptions)
 
 
-########## Abstract inheritance testing
+# ######### Abstract inheritance testing
 
 class AbstractModelATranslationOptions(TranslationOptions):
     fields = ('titlea',)
@@ -107,7 +107,7 @@ class AbstractModelBTranslationOptions(TranslationOptions):
 translator.register(AbstractModelB, AbstractModelBTranslationOptions)
 
 
-########## Fields inheritance testing
+# ######### Fields inheritance testing
 
 class SluggedTranslationOptions(TranslationOptions):
     fields = ('slug',)
@@ -134,7 +134,7 @@ translator.register(Page, PageTranslationOptions)
 translator.register(RichTextPage)
 
 
-########## Manager testing
+# ######### Manager testing
 
 class ManagerTestModelTranslationOptions(TranslationOptions):
     fields = ('title', 'visits', 'description')
@@ -147,7 +147,7 @@ translator.register([CustomManagerTestModel, CustomManager2TestModel],
                     CustomManagerTestModelTranslationOptions)
 
 
-########## TranslationOptions field inheritance testing
+# ######### TranslationOptions field inheritance testing
 
 class FieldInheritanceATranslationOptions(TranslationOptions):
     fields = ['titlea']
@@ -170,14 +170,14 @@ class FieldInheritanceETranslationOptions(FieldInheritanceCTranslationOptions,
     fields = ('titlee',)
 
 
-########## Integration testing
+# ######### Integration testing
 
 class ThirdPartyTranslationOptions(TranslationOptions):
     fields = ('name',)
 translator.register(ThirdPartyRegisteredModel, ThirdPartyTranslationOptions)
 
 
-########## Admin testing
+# ######### Admin testing
 
 class GroupFieldsetsTranslationOptions(TranslationOptions):
     fields = ('title', 'text',)
@@ -189,7 +189,7 @@ class NameTranslationOptions(TranslationOptions):
 translator.register(NameModel, NameTranslationOptions)
 
 
-########## Required fields testing
+# ######### Required fields testing
 
 class RequiredTranslationOptions(TranslationOptions):
     fields = ('non_req', 'req', 'req_reg', 'req_en_reg')
