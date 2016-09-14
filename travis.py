16bfc6d762b7d6661b032f4a5a11570fbd8f348d
Matthias Kestenholz <mk@feinheit.ch>
2016-09-14 11:34:41 +0200

    Fix travis.py for now
    This will fail again as soon as Django 2.0 is released.

--- travis.py
+++ travis.py
@@ -5,5 +5,5 @@ version = sys.argv[1]
 if version.startswith('http'):
     print(version)
 else:
-    next_version = float(version) + 0.1
-    print('Django>=%s,<%.1f' % (version, next_version))
+    next_version = version[:-1] + '%d' % (int(version[-1]) + 1)
+    print('Django>=%s,<%s' % (version, next_version))
