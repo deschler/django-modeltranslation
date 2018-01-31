5cda246b90c0c1871d69934edfed3d9830d75aec
Petr Dlouhý <petr.dlouhy@email.cz>
2018-01-31 12:02:00 +0100

    allow to specify single test in runtests.py

--- runtests.py
+++ runtests.py
@@ -2,13 +2,14 @@
 import os
 import sys
 import warnings
+from optparse import OptionParser
 
 import django
 from django.conf import settings
 from django.core.management import call_command
 
 
-def runtests():
+def runtests(test_path='modeltranslation'):
     if not settings.configured:
         # Choose database for settings
         DATABASES = {
@@ -49,10 +50,13 @@ def runtests():
     django.setup()
     warnings.simplefilter('always', DeprecationWarning)
     failures = call_command(
-        'test', 'modeltranslation', interactive=False, failfast=False, verbosity=2)
+        'test', test_path, interactive=False, failfast=False, verbosity=2)
 
     sys.exit(bool(failures))
 
 
 if __name__ == '__main__':
-    runtests()
+    parser = OptionParser()
+
+    (options, args) = parser.parse_args()
+    runtests(*args)
