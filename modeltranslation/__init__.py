4f5862465aca068fabfdd39b53ef5e7ebdb4491c
Sergey Tereschenko <serg.partizan@gmail.com>
2019-02-13 00:05:28 +0200

    Prepared 0.13-beta2 release

--- modeltranslation/__init__.py
+++ modeltranslation/__init__.py
@@ -3,7 +3,7 @@
 Version code adopted from Django development version.
 https://github.com/django/django
 """
-VERSION = (0, 13, 0, 'beta', 1)
+VERSION = (0, 13, 0, 'beta', 2)
 default_app_config = 'modeltranslation.apps.ModeltranslationConfig'
 
 
