36dec21b438e4b662bb868b21d90995a383b242c
Dirk Eschler <eschler@gmail.com>
2019-04-30 10:57:21 +0200

    Trigger rtd webhook.

--- docs/modeltranslation/conf.py
+++ docs/modeltranslation/conf.py
@@ -15,8 +15,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '../..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 try:
     import modeltranslation
