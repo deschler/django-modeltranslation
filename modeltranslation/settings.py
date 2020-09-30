5fcc4cef84d84e8a5ced1f68493e21251107f6b9
Serg Tereshchenko <serg.partizan@gmail.com>
2020-09-30 23:16:09 +0300

    chore(deps): Update jquery and jquery-ui

--- modeltranslation/settings.py
+++ modeltranslation/settings.py
@@ -78,10 +78,10 @@ LOADDATA_RETAIN_LOCALE = getattr(
 JQUERY_URL = getattr(
     settings,
     "JQUERY_URL",
-    "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js",
+    "//ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js",
 )
 JQUERY_UI_URL = getattr(
     settings,
     "JQUERY_UI_URL",
-    "//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js",
+    "//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js",
 )
