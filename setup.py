1fe45d3b3705fb3153433668aa5033fb4dbb0da7
Serg Tereshchenko <serg.partizan@gmail.com>
2021-04-15 21:52:11 +0300

    chore: Add python 3.9 to supported version list

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Operating System :: OS Independent',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
