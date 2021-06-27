b9cfa294987079948ddda71c673db1fe7dec8ff7
karelhovorka <gpsclock@karel-hovorka.eu>
2021-06-27 20:34:53 +0200

    added css class to main language switch

--- modeltranslation/static/modeltranslation/js/tabbed_translation_fields.js
+++ modeltranslation/static/modeltranslation/js/tabbed_translation_fields.js
@@ -440,7 +440,7 @@ var google, django, gettext;
 
     var MainSwitch = {
       languages: [],
-      $select: $("<select>"),
+      $select: $("<select class='modeltranslation-main-switch'>"),
 
       init: function (groupedTranslations, tabs) {
         var self = this;
