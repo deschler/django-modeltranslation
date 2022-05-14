79d2e089eff2f6bcfd150d3ac6e165bfefa475cb
Serg Tereshchenko <serg.partizan@gmail.com>
2022-05-14 11:23:27 +0300

    fix: Add django version check for default_app_config

--- modeltranslation/__init__.py
+++ modeltranslation/__init__.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+from django import VERSION as _django_version
 
 __version__ = (Path(__file__).parent / "VERSION").open().read().strip()
 
-default_app_config = 'modeltranslation.apps.ModeltranslationConfig'
+if _django_version < (3, 2):
+    default_app_config = 'modeltranslation.apps.ModeltranslationConfig'
