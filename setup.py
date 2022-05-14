626fd79a8c0ce647ddd98f10ae1083841c81870f
Serg Tereshchenko <serg.partizan@gmail.com>
2022-05-14 11:07:32 +0300

    remove seuptools_scm dependency

--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ from setuptools import setup
 
 # (1) check required versions (from https://medium.com/@daveshawley/safely-using-setup-cfg-for-metadata-1babbe54c108)
 pkg_resources.require("setuptools>=39.2")
-pkg_resources.require("setuptools_scm")
 
 setup()
