876f2e715804e5cba9f8dde0b8a75ff3179e908c
Serg Tereshchenko <serg.partizan@gmail.com>
2022-05-14 11:27:19 +0300

    fix: Fix django version detect during install

--- modeltranslation/__init__.py
+++ modeltranslation/__init__.py
@@ -1,7 +1,11 @@
 from pathlib import Path
-from django import VERSION as _django_version
+
+try:
+    from django import VERSION as _django_version
+
+    if _django_version < (3, 2):
+        default_app_config = 'modeltranslation.apps.ModeltranslationConfig'
+except ImportError:
+    pass
 
 __version__ = (Path(__file__).parent / "VERSION").open().read().strip()
-
-if _django_version < (3, 2):
-    default_app_config = 'modeltranslation.apps.ModeltranslationConfig'
