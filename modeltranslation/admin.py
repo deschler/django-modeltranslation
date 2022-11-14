4e9e131019ade2f78644942195825a1585ebea11
Philipp Hafner <phi.hafner@gmail.com>
2022-11-14 14:53:26 +0100

    removed newlines

--- modeltranslation/admin.py
+++ modeltranslation/admin.py
@@ -94,8 +94,6 @@ class TranslationBaseModelAdmin(BaseModelAdmin):
                 db_field.empty_value == 'both' or orig_field.name in self.both_empty_values_fields
             ) and isinstance(field.widget, (forms.TextInput, forms.Textarea)):
                 field.widget = ClearableWidgetWrapper(field.widget)
-            
-
             css_classes = self._get_widget_from_field(field).attrs.get('class', '').split(' ')
             css_classes.append('mt')
             # Add localized fieldname css class
@@ -118,7 +116,6 @@ class TranslationBaseModelAdmin(BaseModelAdmin):
                     # Hide clearable widget for required fields
                     if isinstance(field.widget, ClearableWidgetWrapper):
                         field.widget = field.widget.widget
-            
             self._get_widget_from_field(field).attrs['class'] = ' '.join(css_classes)
 
     def _get_widget_from_field(self, field):
