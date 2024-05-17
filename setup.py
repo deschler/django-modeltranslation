36e9dc36b081d0e33d5c792f739011082819a569
Serhii Tereshchenko <serg.partizan@gmail.com>
2024-05-17 11:05:57 +0300

    chore: Remove deprecated pkg_resources

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python
-import pkg_resources
 from setuptools import setup
-
-# (1) check required versions (from https://medium.com/@daveshawley/safely-using-setup-cfg-for-metadata-1babbe54c108)
-pkg_resources.require("setuptools>=39.2")
 
 setup()
