58894dc8a3553efb0d2f3e8f3f27c0289df23789
Matthias Schoettle <git@mattsch.com>
2024-09-05 16:30:51 -0400

    chore: Add type hint test
    Refs  #758

--- modeltranslation/tests/test_runtime_typing.py
+++ modeltranslation/tests/test_runtime_typing.py
@@ -1,4 +1,9 @@
-from modeltranslation import admin
+import sys
+from typing import get_type_hints
+
+import pytest
+
+from modeltranslation import admin, translator
 from modeltranslation.tests import models
 
 
@@ -10,3 +15,19 @@ def test_translation_admin():
         admin.TranslationInlineModelAdmin[models.ForeignKeyModel, models.TestModel]
     ):
         pass
+
+
+@pytest.mark.skipif(
+    sys.version_info < (3, 10),
+    reason="get_type_hints fails on Python 3.9 despite future annotations",
+)
+@pytest.mark.parametrize(
+    "cls",
+    [
+        translator.TranslationOptions,
+        admin.TranslationAdmin,
+        admin.TranslationInlineModelAdmin,
+    ],
+)
+def test_type_hints(cls):
+    get_type_hints(cls)
