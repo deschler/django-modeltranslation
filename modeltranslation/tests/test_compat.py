880e37e7cd38cade4385cb707156ae401f8891b3
Serhii Tereshchenko <serg.partizan@gmail.com>
2024-09-28 15:11:05 +0300

    chore: Add test-case for #760

---
+++ modeltranslation/tests/test_compat.py
@@ -0,0 +1,21 @@
+# mypy: disable-error-code="import-not-found"
+import pytest
+from modeltranslation import translator
+
+
+class TestDjangoModelUtils:
+    """
+    Test case for https://github.com/deschler/django-modeltranslation/issues/760
+    """
+    def test_soft_deletable_model(self):
+        try:
+            from model_utils.models import SoftDeletableModel
+        except ImportError:
+            pytest.skip("django-model-utils not installed")
+
+        class M1(SoftDeletableModel):
+            pass
+
+        @translator.register(M1)
+        class M1Options(translator.TranslationOptions):
+            pass
