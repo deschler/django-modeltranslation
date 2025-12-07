4ef69d3741900b70028ec6259ded3184c500fa26
rami.boutassghount <ramiboutas@hotmail.com>
2025-12-07 10:51:54 +0100

    fix: add  argument  to   method for Django 6.0 compatibility

--- modeltranslation/manager.py
+++ modeltranslation/manager.py
@@ -375,7 +375,7 @@ class MultilingualQuerySet(QuerySet[_T]):
 
     update.alters_data = True
 
-    def _update(self, values: list[tuple[Field, type[Model] | None, Any]]) -> CursorWrapper:
+    def _update(self, values: list[tuple[Field, type[Model] | None, Any]], returning_fields=None) -> CursorWrapper:
         """
         This method is called in .save() method to update an existing record.
         Here we force to update translation fields as well if the original
@@ -394,7 +394,7 @@ class MultilingualQuerySet(QuerySet[_T]):
                 translation_values.append((translatable_field, model, value))
 
         values += translation_values
-        return super()._update(values)
+        return super()._update(values, returning_fields)
 
     # This method was not present in django-linguo
     @property
